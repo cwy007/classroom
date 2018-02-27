a5710f2607e6605a1f112a70f61ff7e176d7fffd
cwy007 <chanweiyan007@gmail.com>
2018-02-28 01:14:18 +0800

    user must login before accessing courses#new

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
+  sequence(:email) { |n| "user#{n}@example.com" }
+
   factory :user do
-    
+    email
+    password "password"
+    password_confirmation { password }
   end
+
   factory :course do
           title "Course Name"
     description "Description."
