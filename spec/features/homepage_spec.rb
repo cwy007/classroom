1f1f3fee88cf9664d53bbef64081e6cb909588af
cwy007 <chanweiyan007@gmail.com>
2018-02-28 16:17:19 +0800

    test visit homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
 
-RSpec.feature "Home" do
+RSpec.feature "Homepage" do
   scenario "has navbar element" do
     visit root_url
 
     expect(page).to have_css "nav.navbar"
+  end
+
+  scenario "welcomes user" do
+    visit root_url
+
+    expect(page).to have_text "Welcome"
   end
 end
