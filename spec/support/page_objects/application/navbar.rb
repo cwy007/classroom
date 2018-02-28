9dff3d34e98d2880b4a634465c9d1ec5f5769614
cwy007 <chanweiyan007@gmail.com>
2018-02-28 18:01:13 +0800

    refactor user_sign_out_spec

--- spec/support/page_objects/application/navbar.rb
+++ spec/support/page_objects/application/navbar.rb
@@ -5,8 +5,22 @@ module PageObjects
     class Navbar < Base
       def sign_in
         click_on "Login"
-        PageObjects::Devise::Sessions::New.new 
+        PageObjects::Devise::Sessions::New.new
       end
+
+      def sign_out(email)
+        user_dropdown(email).click_on "Logout"
+      end
+
+      def user_dropdown(email)
+        dropdown email
+      end
+
+      private
+
+        def dropdown(text)
+          find ".navbar .dropdown", text: text 
+        end
     end
   end
 end
